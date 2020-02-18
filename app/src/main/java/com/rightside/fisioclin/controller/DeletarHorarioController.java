b6149f13ce0a1759d8136ea9cbc8f5e964c8224e
Matheus Lima <matheusldasilva20088@gmail.com>
2020-02-18 19:56:44 -0300

    ajusta layout ao apagar horario

--- app/src/main/java/com/rightside/fisioclin/controller/DeletarHorarioController.java
+++ app/src/main/java/com/rightside/fisioclin/controller/DeletarHorarioController.java
@@ -3,13 +3,18 @@ package com.rightside.fisioclin.controller;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.media.Image;
+import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.rightside.fisioclin.R;
 import com.rightside.fisioclin.models.Horario;
 import com.rightside.fisioclin.repository.FirebaseRepository;
+import com.rightside.fisioclin.utils.GeralUtils;
 
 
 public class DeletarHorarioController {
@@ -23,31 +28,40 @@ public class DeletarHorarioController {
         TextView horarioData = new TextView(activity);
         TextView horarioDiaSemana = new TextView(activity);
         TextView horarioHora = new TextView(activity);
+        ImageView calendario = new ImageView(activity);
+        ImageView relogio = new ImageView(activity);
+        ImageView calendariot = new ImageView(activity);
         horarioData.setText(horario.getDataFormatada());
-        horarioDiaSemana.setText(horario.getDiaDaSemanaFormatado());
+        horarioDiaSemana.setText(GeralUtils.retornaDiaSemana(horario.getDiaDaSemanaFormatado()));
         horarioHora.setText(horario.getHoraFormatada());
+        relogio.setPadding(10,0,10,0);
+        calendario.setImageResource(R.drawable.ic_date_range_black_24dp);
+        relogio.setImageResource(R.drawable.ic_access_time_black_24dp);
+        calendariot.setImageResource(R.drawable.ic_date_range_black_24dp);
 
-        horarioData.setPadding(50, 10,50,10);
-        horarioData.setTextSize(16);
+
+        horarioData.setTextSize(14);
         horarioData.setTextColor(Color.BLACK);
-        horarioDiaSemana.setPadding(50,10,50,10);
         horarioDiaSemana.setTextColor(Color.BLACK);
-        horarioDiaSemana.setTextSize(16);
-        horarioHora.setPadding(50,10,50,10);
+        horarioDiaSemana.setTextSize(14);
         horarioHora.setTextColor(Color.BLACK);
-        horarioHora.setTextSize(16);
+        horarioHora.setTextSize(14);
 
         LinearLayout container = new LinearLayout(activity.getApplicationContext());
-        container.setOrientation(LinearLayout.VERTICAL);
+        container.setOrientation(LinearLayout.HORIZONTAL);
+        container.setGravity(Gravity.CENTER);
+        container.addView(calendario);
         container.addView(horarioData);
-        container.addView(horarioDiaSemana);
+        container.addView(relogio);
         container.addView(horarioHora);
+        container.addView(calendariot);
+        container.addView(horarioDiaSemana);
 
         alerta.setView(container);
 
 
         
-        alerta.setTitle("Apagar Horário!").setMessage("Tem certeza disso?").setPositiveButton("Apagar", new DialogInterface.OnClickListener() {
+        alerta.setTitle("Excluir Horário").setMessage("Tem certeza que deseja fazer isso?").setPositiveButton("Apagar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 FirebaseRepository.deleteHorarios(horario);
