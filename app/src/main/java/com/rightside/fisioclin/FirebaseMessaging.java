05d5ac5ffc0554e330f51d535e4485d5a821c700
Matheus Lima <matheusldasilva20088@gmail.com>
2020-02-21 18:14:50 -0300

    ajusta layout medico

---
+++ app/src/main/java/com/rightside/fisioclin/FirebaseMessaging.java
@@ -0,0 +1,21 @@
+package com.rightside.fisioclin;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FirebaseMessaging extends FirebaseMessagingService {
+
+
+
+        @Override
+        public void onNewToken(@NonNull String s) {
+            super.onNewToken(s);
+
+            Log.d("teste", s);
+        }
+
+}
+
--- app/src/main/java/com/rightside/fisioclin/MainMedicoActivity.java
+++ app/src/main/java/com/rightside/fisioclin/MainMedicoActivity.java
@@ -3,6 +3,7 @@ package com.rightside.fisioclin;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,13 +19,17 @@ import com.rightside.fisioclin.models.Horario;
 import com.rightside.fisioclin.models.Medico;
 import com.rightside.fisioclin.repository.FirebaseRepository;
 import com.rightside.fisioclin.utils.GeralUtils;
+import com.rightside.fisioclin.viewmodel.ViewModelConsultas;
+import com.rightside.fisioclin.viewmodel.ViewModelFichas;
 
 public class MainMedicoActivity extends FragmentActivity {
 
     private ImageView imageViewDoctorPicture;
     private CardView  cardViewHorarios,cardViewMinhasConsultasMedico, cardViewFichasMedico;
-    private TextView textViewNameDoctor;
+    private TextView textViewNameDoctor, textViewQuantidadeConsultasMarcadas, textViewConsultasFinalizadas;
     private Medico medico;
+    private ViewModelConsultas viewModelConsultas;
+    private ViewModelFichas viewModelFichas;
 
 
     @Override
@@ -37,6 +42,11 @@ public class MainMedicoActivity extends FragmentActivity {
         textViewNameDoctor = findViewById(R.id.textView_name_doctor);
         cardViewMinhasConsultasMedico = findViewById(R.id.card_view_minhas_consultas_medico);
         cardViewFichasMedico = findViewById(R.id.card_view_fichas_medico);
+        textViewConsultasFinalizadas = findViewById(R.id.textViewConsultasFInalizadas);
+        textViewQuantidadeConsultasMarcadas = findViewById(R.id.textViewNumeroConsultas);
+
+        viewModelConsultas = ViewModelProviders.of(this).get(ViewModelConsultas.class);
+        viewModelFichas = ViewModelProviders.of(this).get(ViewModelFichas.class);
 
         FirebaseRepository.getMedico(FirebaseRepository.getIdPessoaLogada()).addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
@@ -45,6 +55,24 @@ public class MainMedicoActivity extends FragmentActivity {
                      medico = documentSnapshot.toObject(Medico.class);
                     alteraInformacaoPerfil(medico);
                 }
+            }
+        });
+
+        viewModelConsultas.getConsultas().observe(this, consultaList -> {
+
+            if(consultaList != null) {
+                textViewQuantidadeConsultasMarcadas.setText(String.valueOf(consultaList.size()));
+            } else {
+                textViewQuantidadeConsultasMarcadas.setText("0");
+            }
+
+        });
+
+        viewModelFichas.getFichas().observe(this, fichaList -> {
+            if(fichaList != null) {
+                textViewConsultasFinalizadas.setText(String.valueOf(fichaList.size()));
+            } else {
+                textViewConsultasFinalizadas.setText("0");
             }
         });
 
